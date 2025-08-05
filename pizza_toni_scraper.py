398be86d8c0aa34ef9b76421b4b3fab909fb6455
alexzuba <loll77@hotmail.it>
2025-08-05 22:20:56 +0200

    Codice praticamente finito

--- pizza_toni_scraper.py
+++ pizza_toni_scraper.py
@@ -1,18 +1,16 @@
+#Parsing HTML
+#Tutorial Beautiful Soup: https://realpython.com/beautiful-soup-web-scraper-python/
 import requests
 from bs4 import BeautifulSoup
-import re
-
-# Import smtplib for the actual sending function
+#Sending Email
+# https://stackoverflow.com/questions/6270782/how-to-send-an-email-with-python
 import smtplib
-
-# Import the email modules we'll need
 from email.mime.text import MIMEText
 
-#Tutorial Beautiful Soup: https://realpython.com/beautiful-soup-web-scraper-python/
-
-
-#Codice per Toni
-
+#Debug constants
+PRINT_DEBUG = False #False -> Niente print di debug
+SEND_EMAIL = True #False -> Non mandare email
+#Email constants
 GMAIL_USERNAME = "pizzeriatonitracker@gmail.com"
 GMAIL_APP_PASSWORD = "vvxcqbaxfvklzxik"
 
@@ -20,57 +18,56 @@ URL = "https://dbuono.com/pizzeria-da-toni-pisa/prodotti/pizze/?products-per-pag
 page = requests.get(URL)
 soup = BeautifulSoup(page.content, "html.parser")
 results = soup.find(id="content") #Funziona, ottengo la parte delle pizze
-#print(results.prettify())
-#print(results)
 pizze_all = results.find_all("ul", class_="woo-product-info")
 
-##FINO A QUA FUNZIONA
-#for pizza in pizze_all:
-#    print(pizza, end="\n" * 2)
+#Printa tutto il codice html di interesse
+if PRINT_DEBUG == True:
+    for pizza in pizze_all:
+        print(pizza, end="\n" * 2)
 
 #Scrivo i nuovi prezzi
-#new_prices = open('new_prices.txt', 'w')
 new_prices = open('new_prices.txt', 'w', encoding='utf-8')
-
 for pizza in pizze_all:
     title_pizza = pizza.find("li", class_="title")
     price_pizza = pizza.find("span", class_="price")
-#    print("TITOLO:")
-#    print(title_pizza.text)
-#    print("PREZZO:")
-#    print(price_pizza.text)
-#    print()
+    #Printa tutte le pizze ed i relativi prezzi
+    if PRINT_DEBUG == True:
+        print("TITOLO:")
+        print(title_pizza.text)
+        print("PREZZO:")
+        print(price_pizza.text)
+        print()
     #Scrivo i nuovi prezzi in new_prices.txt
     new_prices.write(title_pizza.text)
-    #new_prices.write('\n')
     new_prices.write(': ')
     new_prices.write(price_pizza.text)
     new_prices.write('\n')
-
 new_prices.close()
 
 #Confronto con i vecchi prezzi
 old_prices = open('old_prices.txt', 'r')
 new_prices = open('new_prices.txt', 'r')
-changed_prices = open('changed_prices.txt', 'w') #File in cui scrivo i prezzi cambiati, da inviare per mail
+changed_prices = open('changed_prices.txt', 'w', newline='') #File in cui scrivo i prezzi cambiati, da inviare per mail. Non c'è '\n' alla fine della riga scritta
 
 old_prices_data = old_prices.readlines()
 new_prices_data  = new_prices.readlines()
-
 i = 0
 update = 0 #Se rimane a 0 non c'è bisogno di sovrascrivere old_prices, altrimenti devo sovrascriverlo con new_prices
 for line1, line2 in zip(old_prices_data, new_prices_data):
     i += 1
     if line1 == line2:
-        #print(f"Line {i}: IDENTICAL")
-        i = i
+        i = i #Do nothing
     else:
-        print(f"Line {i}:")
-        print(f"\tVecchio prezzo: {line1.strip()}")
-        print(f"\tNuovo prezzo: {line2.strip()}")
+        if PRINT_DEBUG == True:
+            print(f"Line {i}:")
+            print(f"\tVecchio prezzo: {line1.strip()}")
+            print(f"\tNuovo prezzo: {line2.strip()}")
+        changed_prices.write("Vecchio prezzo: ")
+        changed_prices.write(line1)
+        changed_prices.write("Nuovo prezzo: ")
         changed_prices.write(line2)
+        changed_prices.write('\n')
         update = 1
-
 old_prices.close()
 new_prices.close()
 changed_prices.close()
@@ -83,14 +80,10 @@ if update == 1:
     for line in new_prices:
         i += 1
         old_prices.write(line)
-
 old_prices.close()
 new_prices.close()
 
-
-#Mandare email:
-# https://stackoverflow.com/questions/6270782/how-to-send-an-email-with-python
-
+#Mando email
 if update == 1: #Invio mail con il contenuto delle pizze cambiate di prezzo
     changed_prices_path = 'changed_prices.txt'
 
@@ -98,21 +91,19 @@ if update == 1: #Invio mail con il contenuto delle pizze cambiate di prezzo
         lines = file.readlines()
         file_content = ''.join(lines)
 
-#    print(file_content)
+    if PRINT_DEBUG == True:
+        print(file_content)
 
-    recipients = ["loll77@hotmail.it"]
-    msg = MIMEText(file_content)
-    msg["Subject"] = "Prezzo delle pizze di Toni cambiato!!"
-    msg["To"] = ", ".join(recipients)
-    msg["From"] = f"{GMAIL_USERNAME}@gmail.com"
-
-    smtp_server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-    smtp_server.login(GMAIL_USERNAME, GMAIL_APP_PASSWORD)
-    smtp_server.sendmail(msg["From"], recipients, msg.as_string())
-    smtp_server.quit()
-
-
-#Miglioramenti: Quando invia la mail, non mandare soltanto il nuovo prezzo, ma anche quello vecchio
+    if SEND_EMAIL == True:
+        recipients = ["loll77@hotmail.it", "alessandrozubani98@gmail.com"]
+        msg = MIMEText(file_content)
+        msg["Subject"] = "Prezzo delle pizze di Toni cambiato!!"
+        msg["To"] = ", ".join(recipients)
+        msg["From"] = f"{GMAIL_USERNAME}@gmail.com"
+        smtp_server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        smtp_server.login(GMAIL_USERNAME, GMAIL_APP_PASSWORD)
+        smtp_server.sendmail(msg["From"], recipients, msg.as_string())
+        smtp_server.quit()
 
 
 
